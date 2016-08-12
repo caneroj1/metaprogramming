1f36c0bd0285f9dc13d70f6780ab851325e61826
caneroj1 <caneroj1@tcnj.edu>
2016-08-11 21:17:06 -0400

    Initial Commit

---
+++ MetaProgramming/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MetaProgramming
+//
+//  Created by Joseph Canero on 8/11/16.
+//  Copyright © 2016 jcanero. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
